333c365ed8bf0c0848c5d75eb24accaa49b9c2b1
Mihai Dima <dimamihaimail@gmail.com>
2023-12-01 14:49:19 +0000

    first commit

---
+++ src/main/java/com/malibentoeventservice/malibentoeventservice/MalibentoeventserviceApplication.java
@@ -0,0 +1,13 @@
+package com.malibentoeventservice.malibentoeventservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalibentoeventserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalibentoeventserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malibentoeventservice/malibentoeventservice/MalibentoeventserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malibentoeventservice.malibentoeventservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalibentoeventserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
