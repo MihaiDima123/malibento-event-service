1b845f295364de8ebc7f02734a1d0620db845af7
Mihai Dima <dimamihaimail@gmail.com>
2023-12-25 13:00:44 +0200

    MAL-3: enable transactional for create, edit and remove event

--- src/main/java/com/malibentoeventservice/malibentoeventservice/service/EventService.java
+++ src/main/java/com/malibentoeventservice/malibentoeventservice/service/EventService.java
@@ -1,10 +1,14 @@
 package com.malibentoeventservice.malibentoeventservice.service;
 
 import com.malibentoeventservice.malibentoeventservice.entities.Event;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface EventService {
     Event getEventById(final Integer eventId);
+    @Transactional
     Event createEvent(final Event event);
+    @Transactional
     Event editEvent(final Event event);
+    @Transactional
     void removeEvent(final Integer eventId);
 }
