b5ede5b81106ae448e24ff86a9e5f39aa6841767
Mihai Dima <dimamihaimail@gmail.com>
2023-12-25 22:06:51 +0200

    MAL-3: code cleanup

--- src/main/java/com/malibentoeventservice/malibentoeventservice/configuration/FilterRegistrationConfiguration.java
+++ src/main/java/com/malibentoeventservice/malibentoeventservice/configuration/FilterRegistrationConfiguration.java
@@ -1,8 +1,6 @@
 package com.malibentoeventservice.malibentoeventservice.configuration;
 
 import com.malibentoeventservice.malibentoeventservice.configuration.filters.BeforeAnyRequestFilter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
